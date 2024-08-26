e94243e273e0206a60f0593bcdf8e8ec26f43630
Azizmatov Sharofiddin <azizmatov1202@gmail.com>
2024-08-27 01:24:05 +0300

    init

---
+++ src/lib/shared/shared.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'asigno-shared',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './shared.component.html',
+  styleUrl: './shared.component.scss',
+})
+export class SharedComponent {}
